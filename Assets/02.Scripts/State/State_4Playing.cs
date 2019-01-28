fb3ea3d187ef6c8ec3f982cccf4c5405c0855c9e
yeon <yeonyya@gmail.com>
2019-01-28 19:35:22 +0900

    상태2와 상태4에서 타이머 완료

--- Assets/02.Scripts/State/State_4Playing.cs
+++ Assets/02.Scripts/State/State_4Playing.cs
@@ -7,9 +7,6 @@ namespace Yeon
 {
     public class State_4Playing : StateBase
     {
-        //테스트용 타임 수연이가 만든 타이머에서 가져와야함
-        public float time = 20f;
-
         private float totalTime = 20f;
         private bool isGameOver = false;
 
@@ -30,16 +27,15 @@ namespace Yeon
             base.Execute();
             Debug.Log("4번 실행중...");
             var score = GameObject.Find("ScoreCheck").GetComponent<ScoreCheck>();
-            if (score.CheckingNextState(time) == true)
-            {
-                GetNextState();
-            }
 
-            //내가 추가 // 다시볼것
             if (!isGameOver && totalTime > 0)
             {
                 totalTime -= Time.deltaTime;
                 RefCtr.instance.showTime.text = Mathf.Round(totalTime) + "";
+            }
+            else
+            {
+                StateMachine.ChangeState(GetNextState());
             }
         }
 
