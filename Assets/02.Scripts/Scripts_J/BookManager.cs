885713e57a2ce25918825a65ef17dc0824f6df80
Jimoon-Kim <wlansdl@gmail.com>
2019-01-29 18:16:53 +0900

    stagemanager 에러수정
    bookmanager의 language는 모두 대문자로

--- Assets/02.Scripts/Scripts_J/BookManager.cs
+++ Assets/02.Scripts/Scripts_J/BookManager.cs
@@ -23,7 +23,7 @@ public class BookManager : MonoBehaviour
     }
 
     public Color[] colors = { Color.white, Color.green, Color.red, Color.blue, Color.yellow, new Color(0.8f, 0.4f, 0f), Color.grey, new Color(1f, 0.4f, 1f) };
-    public string[] Language = { "Korean", "English", "Chinese", "Japanese", "Greek", "Russian", "Arabian", "Mongolian" };
+    public string[] Language = { "KOREAN", "ENGLISH", "CHINESE", "JAPANESE", "GREEK", "RUSSIAN", "ARABIAN", "MONGOLIAN" };
     
     //정답으로 인정하는 거리와 각도
     public float correctDist = 0.1f;
@@ -45,11 +45,16 @@ public class BookManager : MonoBehaviour
     {
         foreach (var item in books)
         {
-            item.SetColor(colors[Random.Range(0, colorVariety)]);
-            var lang = Language[Random.Range(0, langVariety)];
-            var index = Random.Range(0, BookManager.Instance.titleVariety);
-            var list = BookManager.Instance.booksName[index];
-            item.SetText((list[lang]).ToString());
+            //랜덤 색 전달
+            var coloridx = colors[Random.Range(0, colorVariety)];
+            item.SetColor(coloridx);
+
+            //랜덤 언어의 랜덤 제목 전달
+            var lang = Language[Random.Range(0, langVariety)];    //랜덤 언어 설정
+            var index = Random.Range(0, titleVariety);            //제목 수 중 랜덤인덱스
+            var list = booksName[index];                          //딕셔너리의 리스트 중 책 제목의 인덱스
+            item.SetText((list[lang]).ToString());                //해당 인덱스에 해당 언어 전달.
+            
         }
     }
 
