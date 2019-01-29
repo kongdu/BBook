477b9fa1555cf8317e282ade38c89172035e571e
wkdeo12 <buronson@naver.com>
2019-01-29 11:29:17 +0900

    다시
    복구

--- Assets/02.Scripts/Scripts_J/StageManager.cs
+++ Assets/02.Scripts/Scripts_J/StageManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class StageManager : MonoBehaviour
 {
     private static StageManager _instance = null;
+
     public static StageManager Instance
     {
         get
@@ -31,6 +32,7 @@ public class StageManager : MonoBehaviour
 
     //밸런스 수치
     public float announceTime = 5f;
+
     public float deadline = 20f;
     public float scorePerTime = 10;
     public float scorePerStage = 2;
@@ -38,13 +40,13 @@ public class StageManager : MonoBehaviour
 
     public int stage = 1;
     public int booksToClear;
-    
+
     public List<Dictionary<string, object>> stageData = null;
 
-    float currTime;
-    float score = 0;
-    Coroutine timeRoutine;
-    StageState _state = StageState.PRESTAGE;
+    private float currTime;
+    private float score = 0;
+    private Coroutine timeRoutine;
+    private StageState _state = StageState.PRESTAGE;
 
     private void Awake()
     {
@@ -63,7 +65,7 @@ public class StageManager : MonoBehaviour
         StartCoroutine(GameStart());
     }
 
-    IEnumerator GameStart()
+    private IEnumerator GameStart()
     {
         currTime = deadline;
         booksToClear = (int)stageData[stage]["DropBooks"];
@@ -77,7 +79,7 @@ public class StageManager : MonoBehaviour
         StartStage();
     }
 
-    void ShowAnnounce()
+    private void ShowAnnounce()
     {
         announcePanel.SetActive(true);
     }
@@ -94,18 +96,18 @@ public class StageManager : MonoBehaviour
             StageOver();
         }
     }
-    
+
     /// <summary>
     /// 지진이펙트를 부르고, 거기에 떨어뜨릴 책의 수를 전달.
     /// </summary>
-    IEnumerator CallEarthQuake()
+    private IEnumerator CallEarthQuake()
     {
         //전달할 int는 (int)stageData[stage][DropBooks]
         yield return new WaitForSeconds(2f);
         Debug.Log("지진 끝");
     }
 
-    void StartStage()
+    private void StartStage()
     {
         currTime = deadline;
         timeText.text = currTime.ToString("00.00");
@@ -114,9 +116,9 @@ public class StageManager : MonoBehaviour
         timeRoutine = StartCoroutine(TimeCheck());
     }
 
-    IEnumerator TimeCheck()
+    private IEnumerator TimeCheck()
     {
-        if(_state==StageState.INGAME)
+        if (_state == StageState.INGAME)
         {
             while (currTime >= 0)
             {
