6bc8b6b62c6b9b4028b9c98ab6183cbcec5018e9
wkdeo12 <buronson@naver.com>
2019-01-29 11:40:58 +0900

    나의씬추가및스테이지메니저 오류나는 부분 일단 주석처리
    스테이지메니저의 오류나는 부분 일단 주석 처리

--- Assets/02.Scripts/Scripts_J/StageManager.cs
+++ Assets/02.Scripts/Scripts_J/StageManager.cs
@@ -93,7 +93,7 @@ public class StageManager : MonoBehaviour
         booksToClear--;
         if (booksToClear == 0)
         {
-            StageOver();
+            //StageOver();
         }
     }
 
